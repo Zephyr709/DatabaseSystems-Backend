50e72e06dc5ec274b90dd56ee168ee1acbefed59
Jager Cooper <100608609+Zephyr709@users.noreply.github.com>
2024-10-27 11:31:31 -0230

    more init (#4)

--- app/routers/database.py
+++ app/routers/database.py
@@ -4,6 +4,10 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-router.post("/items/")
-def create_item():
+@router.post("/items/")
+async def create_item():
     return {"message": "Item created successfully"}
+
+@router.get("/items")
+async def read_items():
+    return [{"name": "Item One"}, {"name": "Item Two"}]
